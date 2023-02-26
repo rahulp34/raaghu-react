b1e6de56e1cabc3e32c029fbc2f2edbd8dd40585
Maithili-Londhe <maithili.londhe@waiin.com>
2023-02-27 00:05:57 +0530

    setting page functionality.

--- raaghu-mfe/rds_pages/rds-page-settings/src/Settings/Settings.tsx
+++ raaghu-mfe/rds_pages/rds-page-settings/src/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   RdsButton,
   RdsFabMenu,
@@ -6,156 +6,332 @@ import {
   RdsOffcanvas,
 } from "../../../../../raaghu-elements/src/index";
 import {
-  RdsCompUserManagement,
-  RdsCompOtherSettings,
-  RdsCompTenantManagement,
+  RdsCompIdentityManagement,
   RdsCompEmail,
-  RdsCompInvoice,
-  RdsCompSecurity,
+  RdsCompAccount,
+  RdsCompFeatureManagement,
 } from "../../../rds-components";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../../../libs/state-management/hooks";
+import {
+  fetchAccountGeneralSettings,
+  fetchCaptchaSettings,
+  fetchEmailSettings,
+  fetchExternalProviderSettings,
+  fetchFeaturesSettings,
+  fetchIdentitySettings,
+  fetchTwoFactorSettings,
+  restoreToDefaultFeaturesSettings,
+  saveAccountCaptchaSettings,
+  saveAccountExternalProviderSettings,
+  saveAccountGeneralSettings,
+  saveAccountTwoFactorSettings,
+  saveEmailSettings,
+  saveFeaturesSettings,
+  saveIdentitySettings,
+} from "../../../../libs/state-management/settings/settings-slice";
+
 interface RdsCompSettingsProps {}
 const navtabsItems = [
-  { label: "Tenant Management", tablink: "#nav-home", id: 0 },
-  { label: "User Management", tablink: "#nav-profile", id: 1 },
-  { label: "Security", tablink: "#nav-profile", id: 2 },
-  { label: "Email(SMTP)", tablink: "#nav-profile", id: 3 },
-  { label: "Invoice", tablink: "#nav-profile", id: 4 },
-  { label: "Other Settings", tablink: "#nav-profile", id: 5 },
+  { label: "Email Settings", tablink: "#nav-home", id: 0 },
+  { label: "Identity Management", tablink: "#nav-profile", id: 1 },
+  { label: "Account", tablink: "#nav-profile", id: 2 },
+  { label: "Feature Management", tablink: "#nav-profile", id: 3 },
 ];
-const userManagementSettings = [
-  {
-    id: 1,
-    label: "Email Confirmation Required For Login.",
-    checked: false,
-  },
-  {
-    id: 2,
-    label: "Phone Number Verification Enabled (Via SMS)",
-    checked: false,
-  },
-  {
-    id: 3,
-    label: "Use Security Image Question (Captcha) On Login.",
-    checked: false,
-  },
-  {
-    id: 4,
-    title: "Cookie Consent",
-    label: "Cookie Consent Enabled",
-    checked: false,
-  },
-  {
-    id: 5,
-    title: "Session TimeOut Control",
-    label: "Session Time Out Control Enabled",
-    checked: false,
-  },
-  {
-    id: 6,
-    title: "Profile",
-    label: "Allow Using to use Gravatar Profile Picture",
-    checked: false,
-  },
-];
-
 const Settings = (props: RdsCompSettingsProps) => {
+  const data = useAppSelector((state) => state.persistedReducer.settings);
   const [activeNavTabId, setActiveNavTabId] = useState(0);
-  const editionDropdownListItems = [
-    {
-      label: "Standard",
-      val: "en",
+  const [emailSettings, setEmailSettings] = useState<any>({
+    defaultFromDisplayName: "",
+    defaultFromAddress: "",
+    smtpHost: "",
+    smtpPort: "",
+    smtpEnableSsl: false,
+    smtpUseDefaultCredentials: false,
+    smtpUserName: "",
+    smtpPassword: "",
+    smtpDomain: "",
+  });
+  const [identitySettings, setIdentitySettings] = useState<any>({
+    lockout: {
+      allowedForNewUsers: false,
+      lockoutDuration: 10,
+      maxFailedAccessAttempts: "",
     },
-    {
-      label: "Basic",
-      val: "en",
+    password: {
+      requiredLength: "",
+      requiredUniqueChars: "",
+      requireDigit: false,
+      requireNonAlphanumeric: false,
+      requireUppercase: false,
+      requireLowercase: false,
     },
-    {
-      label: "Premium",
-      val: "en",
+    signIn: {
+      requireConfirmedEmail: false,
+      requireConfirmedPhoneNumber: false,
+      enablePhoneNumberConfirmation: false,
     },
-    {
-      label: "Professional",
-      val: "en",
+    user: {
+      isEmailUpdateEnabled: false,
+      isUserNameUpdateEnabled: false,
     },
-  ];
-  const checkgroupList = [
-    {
-      id: 1,
-      label: "Require Digit",
-      checked: false,
-      disabled: false,
-    },
-    {
-      id: 2,
-      label: "Require Lowercase",
-      checked: false,
-      disabled: false,
-    },
-    {
-      id: 3,
-      label: "Require Non-Alphanumeric",
-      checked: false,
-      disabled: false,
-    },
-    {
-      id: 4,
-      label: "Require Uppercase",
-      checked: false,
-      disabled: false,
-    },
-  ];
+  });
+  const [accountGeneralSettings, setAccountGeneralSettings] = useState<any>({
+    enableLocalLogin: false,
+    isSelfRegistrationEnabled: false,
+  });
+  const [accountTwoFactorSettings, setAccountTwoFactSettings] = useState<any>({
+    isRememberBrowserEnabled: false,
+    twoFactorBehaviour: 1,
+    usersCanChange: false,
+  });
+  const [accountCaptchaSettings, setAccountCaptchaSettings] = useState<any>({
+    score: "",
+    siteKey: "",
+    siteSecret: "",
+    useCaptchaOnLogin: false,
+    useCaptchaOnRegistration: false,
+    verifyBaseUrl: "",
+    version: "",
+  });
+  const [externalProviderSettings, setExternalProviderSettings] = useState<any>(
+    [
+      {
+        enabled: false,
+        name: "google",
+        properties: [{ name: "ClientId", value: "" }],
+        secretProperties: [{ name: "clientSecret", value: "" }],
+      },
+      {
+        enabled: false,
+        name: "microsoft",
+        properties: [{ name: "ClientId", value: "" }],
+        secretProperties: [{ name: "clientSecret", value: "" }],
+      },
+      {
+        enabled: false,
+        name: "twitter",
+        properties: [{ name: "ConsumerId", value: "" }],
+        secretProperties: [{ name: "ConsumerSecret", value: "" }],
+      },
+    ]
+  );
+
+  const [featureIdentitySettingsData, setfeatureIdentitySettings] =
+    useState<any>([
+      { value: "Optional" },
+      { value: 8 },
+      { value: true },
+      { value: true },
+      { value: true },
+      { value: true },
+      { value: true },
+      { value: true },
+    ]);
+
+  const dispatch = useAppDispatch();
+  function handleEmailSubmit(formData: any) {
+    console.log("function of parent component", formData);
+    dispatch(saveEmailSettings(formData) as any);
+  }
+  function saveIdentityData(data: any) {
+    dispatch(saveIdentitySettings(data) as any);
+  }
+
+  function saveAccountData(data: any) {
+    dispatch(saveAccountGeneralSettings(data.accountGeneralData) as any);
+    dispatch(saveAccountTwoFactorSettings(data.twoFactorData) as any);
+    dispatch(saveAccountCaptchaSettings(data.accountCaptchData) as any);
+    dispatch(saveAccountExternalProviderSettings(data.externalProvider) as any);
+  }
+
+  useEffect(() => {
+    
+    dispatch(fetchEmailSettings() as any);
+
+    dispatch(fetchIdentitySettings() as any);
+
+    dispatch(fetchAccountGeneralSettings() as any);
+
+    dispatch(fetchTwoFactorSettings() as any);
+
+    dispatch(fetchCaptchaSettings() as any);
+
+    dispatch(fetchExternalProviderSettings() as any);
+
+    dispatch(fetchFeaturesSettings() as any);
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (data.emailSettings) {
+      setEmailSettings(data.emailSettings);
+    }
+  }, [data.emailSettings]);
+
+  useEffect(() => {
+    if (data.identitySettings) {
+      setIdentitySettings(data.identitySettings);
+    }
+  }, [data.identitySettings]);
+
+  useEffect(() => {
+    if (data.accountGeneralSettings) {
+      setAccountGeneralSettings(data.accountGeneralSettings);
+    }
+  }, [data.accountGeneralSettings]);
+
+  useEffect(() => {
+    if (data.accountTwoFactorSettings) {
+      setAccountTwoFactSettings(data.accountTwoFactorSettings);
+    }
+  }, [data.accountTwoFactorSettings]);
+
+  useEffect(() => {
+    if (data.accountCaptchaSettings) {
+      setAccountCaptchaSettings(data.accountCaptchaSettings);
+    }
+  }, [data.accountCaptchaSettings]);
+
+  useEffect(() => {
+    if (data.accountExternalProvider) {
+      let a = JSON.parse(
+        JSON.stringify(data.accountExternalProvider.settings[0])
+      );
+      let b = JSON.parse(
+        JSON.stringify(data.accountExternalProvider.settings[1])
+      );
+      let c = JSON.parse(
+        JSON.stringify(data.accountExternalProvider.settings[2])
+      );
+      let temp = [];
+      temp.push(a);
+      temp.push(b);
+      temp.push(c);
+      setExternalProviderSettings(temp);
+    }
+  }, [data.accountExternalProvider]);
+
+  useEffect(() => {
+    if (data.featureIdentitySettings) {
+      let tempFeatureData: any[] = [];
+      data.featureIdentitySettings.groups.map((item: any) => {
+        item.features.map((e: any) => {
+          let data: any = {};
+          if (e.value == "true" || e.value == "True") {
+            data = {
+              name: e.name,
+              value: true,
+            };
+          } else if (e.value == "False" || e.value == "false") {
+            data = {
+              name: e.name,
+              value: false,
+            };
+          } else {
+            data = {
+              name: e.name,
+              value: e.value,
+            };
+          }
+          tempFeatureData.push(data);
+        });
+      });
+      console.log("features  data", tempFeatureData);
+      // compData = tempFeatureData;
+      setfeatureIdentitySettings(tempFeatureData);
+    }
+  }, [data.featureIdentitySettings]);
+
+  function saveFeature(data: any) {
+    debugger;
+    const tempData :any[] = []
+    data.map((e:any) => {
+      const item = {
+        value : String(e.value),
+        name : e.name
+      }
+      tempData.push(item);
+    })
+    dispatch(saveFeaturesSettings({ features: tempData }) as any);
+  }
+
+  function restoreFeatures(data: any) {
+    debugger;
+    dispatch(restoreToDefaultFeaturesSettings(data) as any).then((res: any) => {
+      dispatch(fetchFeaturesSettings() as any);
+    });
+  }
+
   return (
     <div className="tenant">
-      <div className="d-flex justify-content-end pb-3">
-        <RdsButton
-          icon="plus"
-          iconColorVariant="light"
-          size="small"
-          type="button"
-          colorVariant="primary"
-          label="SAVE ALL"
-          iconHeight="15px"
-          iconWidth="15px"
-          iconFill={false}
-          iconStroke={true}
-        />
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card border-0 py-2 px-4 h-100 rounded-0 card-full-stretch">
+            <RdsNavtabs
+              navtabsItems={navtabsItems}
+              type="tabs"
+              activeNavtabOrder={(activeNavTabId) => {
+                setActiveNavTabId(activeNavTabId);
+              }}
+            />
+            {activeNavTabId == 0 && (
+              <RdsCompEmail
+                handleSubmit={(formData: any) => {
+                  handleEmailSubmit(formData);
+                }}
+                emailSettings={emailSettings}
+              />
+            )}
+
+            {activeNavTabId == 1 && (
+              <RdsCompIdentityManagement
+                handleIdentity={(data: any) => {
+                  saveIdentityData(data);
+                }}
+                lockoutSettings={identitySettings.lockout}
+                passwordSettings={identitySettings.password}
+                signSettings={identitySettings.signIn}
+                userSettings={identitySettings.user}
+              />
+            )}
+            {activeNavTabId == 2 && (
+              <RdsCompAccount
+                versionList={[
+                  { option: "2", value: 2 },
+                  { option: "3", value: 3 },
+                ]}
+                twoFactList={[
+                  { option: "Optional", value: 0 },
+                  { option: "Disabled", value: 1 },
+                  { option: "Forced", value: 2 },
+                ]}
+                accountGeneralSettings={accountGeneralSettings}
+                accountTwoFactorSettings={accountTwoFactorSettings}
+                accountCaptchaSettings={accountCaptchaSettings}
+                accountExternalProvider={externalProviderSettings}
+                handleAccountSettings={(data: any) => {
+                  saveAccountData(data);
+                }}
+              />
+            )}
+            {activeNavTabId == 3 && (
+              <RdsCompFeatureManagement
+                featureIdentitySettingsData1={featureIdentitySettingsData}
+                twoFactorList={[
+                  { option: "Optional", value: "Optional" },
+                  { option: "Disabled", value: "Disabled" },
+                  { option: "Forced", value: "Forced" },
+                ]}
+                saveFeature={saveFeature}
+                restoreFeatures={restoreFeatures}
+              />
+            )}
+          </div>
+        </div>
       </div>
-<div className="row">
-<div className="col-md-12">
-  <div className="card border-0 py-2 px-4 h-100 rounded-0 card-full-stretch">
-  <RdsNavtabs
-        navtabsItems={navtabsItems}
-        type="tabs"
-        activeNavtabOrder={(activeNavTabId) => {
-          setActiveNavTabId(activeNavTabId);
-        }}
-      />
-      {activeNavTabId == 0 && (
-        <RdsCompTenantManagement
-          settingsTenantEditionList={[]}
-          allowSelfRegistration={true}
-          useCaptchaOnRegistration={true}
-          isNewRegisteredTenantActiveByDefault={false}
-        />
-      )}
-      {activeNavTabId == 1 && (
-        <RdsCompUserManagement
-          Usermanagementsettings={userManagementSettings}
-        />
-      )}
-      {activeNavTabId == 2 && (
-        <RdsCompSecurity checkgroupList={checkgroupList} />
-      )}
-      {activeNavTabId == 3 && <RdsCompEmail />}
-      {activeNavTabId == 4 && <RdsCompInvoice />}
-      {activeNavTabId == 5 && <RdsCompOtherSettings />}
-  </div>
-  
-  </div>
-  </div>
-</div>
-   
+    </div>
   );
 };
-
 export default Settings;
