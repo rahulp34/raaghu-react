bb7a7d6b94a97aded03602b9fc3ac531b8ccb534
Alok Nakhate <alok.nakhate@waiin.com>
2023-03-16 11:01:43 +0530

    Update rds-comp-feature-management.tsx

--- raaghu-components/src/rds-comp-feature-management/rds-comp-feature-management.tsx
+++ raaghu-components/src/rds-comp-feature-management/rds-comp-feature-management.tsx
@@ -7,7 +7,7 @@ import {
   RdsLabel,
   RdsNavtabs,
   RdsSelectList,
-} from "../rds-elements";
+} from "raaghu-react-elements";
 
 export interface RdsCompFeatureManagementProps{
  featureIdentitySettingsData1?:any;
