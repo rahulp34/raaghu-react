c01ed2bd500d641ba896d04b6b9dacc441c5bf8e
Bhorga <harshit.bhorga@waiin.com>
2023-03-18 18:45:24 +0530

    roles page functionality completed

--- raaghu-components/src/rds-comp-claims/rds-comp-claims.tsx
+++ raaghu-components/src/rds-comp-claims/rds-comp-claims.tsx
@@ -2,11 +2,19 @@ import React, { useState, useEffect, Children, useReducer } from "react";
 import RdsCompAlertPopup from "../rds-comp-alert-popup";
 import RdsCompDatatable from "../rds-comp-data-table";
 import RdsCompDataTable from "../rds-comp-data-table";
-import { RdsButton, RdsIcon, RdsInput, RdsLabel, RdsSelectList } from "../rds-elements";
+import {
+  RdsButton,
+  RdsIcon,
+  RdsInput,
+  RdsLabel,
+  RdsSelectList,
+} from "../rds-elements";
 import "./rds-comp-claims.scss";
 
 export interface RdsCompClaimsProps {
-  TypeList: any[];
+  allClaimsArray?: any[];
+  claimsTable?: any[];
+  id?:any;
 }
 
 export interface SelectedItem {
@@ -15,52 +23,33 @@ export interface SelectedItem {
 }
 
 const RdsCompClaims = (props: RdsCompClaimsProps) => {
+  const [allClaimsArray, setAllClaimsArray] = useState<any>(props.allClaimsArray)
   const [selectedData, setSelectedData] = useState<any>({
-    id:0,
-    claimType:'',
-    claimValue:''
+    id: 0,
+    claimType: "",
+    claimValue: "",
+    roleId: props.id,
   });
-  const [tableData ,setTableData  ] = useState<any>([])
-  // useEffect(() => {
-  //   setTableData(selectedData)
-  // },[tableData])
-  // const [value, setValue] = useState(props.selectValue);
-  // useEffect(() => {
-  //   setValue(props.selectValue);
-  // }, [props.selectValue]);
+  const [tableData, setTableData] = useState<any>(props.claimsTable);
+  const actions= [{ id: "delete", displayName: "delete" }];
 
-  // const [selectedItems, setSelectedItems] = useState<SelectedItem[]>([]);
   const handleAddItem = () => {
-  
+    debugger
     let newTempData: any;
     newTempData = {
       id: selectedData.id,
       claimType: selectedData.claimType,
       claimValue: selectedData.claimValue,
-      delete: (<>
-       <a
-                    className="mx-2"
-                     type="button"   
-                     data-bs-toggle="modal"
-                     data-bs-target={`#deleteTreeNode`}
-                     onClick={HandlerDelete}
-                     >
-                     <RdsIcon 
-                       name={"delete"}
-                       height="16px"
-                       width="20px"
-                       stroke={true}
-                     ></RdsIcon>
-                   </a>
-      </>),
+      roleId: props.id,
+      delete: "Delete",
     };
     setTableData((prev: any) => [...prev, newTempData]);
-setSelectedData({...selectedData, id: selectedData.id +1})
-};
+    setSelectedData({ ...selectedData, id: selectedData.id + 1 });
+  };
 
-const HandlerDelete =()=>{
-  console.log(' yes bhai you clicked me !!')
-}
+  const HandlerDelete = () => {
+    console.log(" yes bhai you clicked me !!");
+  };
   // const handleDeleteItem = (index: number) => {
   //   setSelectedItems(selectedItems.filter((item, i) => i !== index));
   // };
@@ -76,43 +65,26 @@ const HandlerDelete =()=>{
       key: "claimValue",
       datatype: "text",
       sortable: true,
-    },   {
-      displayName: "Action",
-      key: "delete",
-      datatype: "children",
-      sortable: true,
-    },
+    }
   ];
-  const onDeleteHandler=(()=>{
-    let tempdata:any[]=[];
-    tableData.forEach((element:any)=>{
-      if(element.value!= tableData.value){
-        tempdata.push(tableData);
-      }
-    })
-    
-
-  })
+  
+  function onActionSelection(data:any){
+    debugger
+    console.log(data)
+  }
 
   return (
     <>
       <div className="form">
-        <div className="col md-1">
-          <label htmlFor="claims">Claims</label>
-        </div>
-
         <div className="row">
           <div className="col-md-5 mt-3">
-          <RdsLabel label="Claims Type" class="pb-2" />
+            <RdsLabel label="Claims Type" class="pb-2" />
             <RdsSelectList
               label="Select"
-              selectItems={[
-                { option: "optional", value: "Optional" },
-                { option: "forced", value: "Forced" },
-                { option: "disable", value: "Disable" },
-              ]}
+              selectItems={allClaimsArray}
               selectedValue={selectedData.claimType}
-              onSelectListChange={(e: any) => {setSelectedData({ ...selectedData, claimType:e });
+              onSelectListChange={(e: any) => {
+                setSelectedData({ ...selectedData, claimType: e });
               }}
               size="small"
             ></RdsSelectList>
@@ -125,26 +97,29 @@ const HandlerDelete =()=>{
               placeholder="Enter Value"
               name="value"
               value={selectedData.claimValue}
-              onChange={(event) => setSelectedData({...selectedData, claimValue:event.target.value})}
+              onChange={(event) =>
+                setSelectedData({
+                  ...selectedData,
+                  claimValue: event.target.value,
+                })
+              }
             ></RdsInput>
           </div>
 
-          
-            <div className="col-md-2 mt-4 pt-4">
-              <RdsButton
-                type={"button"}
-                label="Add"
-                icon="plus"
-                iconHeight="15px"
-                onClick={handleAddItem}
-                class="text-start"
-                iconColorVariant="dark"
-                colorVariant="primary"
-                size="small"
-              ></RdsButton>
-            </div>
+          <div className="col-md-2 mt-4 pt-4">
+            <RdsButton
+              type={"button"}
+              label="Add"
+              icon="plus"
+              iconHeight="15px"
+              onClick={handleAddItem}
+              class="text-start"
+              iconColorVariant="dark"
+              colorVariant="primary"
+              size="small"
+            ></RdsButton>
+          </div>
         </div>
-
 
         <div className="row mt-3 ">
           <RdsCompDatatable
@@ -152,32 +127,11 @@ const HandlerDelete =()=>{
             tableData={tableData}
             pagination={true}
             recordsPerPage={10}
+            actions={actions}
             recordsPerPageSelectListOption={true}
-           // onActionSelection={onActionSelection}
+            onActionSelection={onActionSelection}
           ></RdsCompDatatable>
           {/* <RdsCompAlertPopup alertID="Del" onSuccess={onDeleteHandler} /> */}
-
-        </div>
-
-        <div className="footer-buttons mb-2 d-flex">
-          <RdsButton
-            class="me-2"
-            tooltipTitle={""}
-            type={"button"}
-            label="Cancel"
-            colorVariant="outline-primary"
-            size="small"
-            databsdismiss="offcanvas"
-          ></RdsButton>
-          <RdsButton
-            class="me-2"
-            label="Create"
-            size="small"
-            colorVariant="primary"
-            tooltipTitle={""}
-            type={"submit"}
-            databsdismiss="offcanvas"
-          ></RdsButton>
         </div>
       </div>
     </>
