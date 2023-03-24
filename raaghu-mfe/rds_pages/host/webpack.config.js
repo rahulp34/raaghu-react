7a54e9f02be9b532681ff7123873af6926dea58e
Alok Nakhate <alok.nakhate@waiin.com>
2023-03-24 12:26:14 +0530

    Resolved build issues

--- raaghu-mfe/rds_pages/host/webpack.config.js
+++ raaghu-mfe/rds_pages/host/webpack.config.js
@@ -130,7 +130,7 @@ module.exports = (env, argv) => {
           SecurityLogs: mfeConfigJSON["securityLogs"].url,
           Chats: mfeConfigJSON["chats"].url,
           FileManagement: mfeConfigJSON["fileManagement"].url,
-          Forms : mfeConfigJSON["forms"].url,
+          Forms: mfeConfigJSON["forms"].url,
           Blogger: mfeConfigJSON["blogger"].url,
           IdentityResources: mfeConfigJSON["identityResources"].url,
           Client: mfeConfigJSON["client"].url,
@@ -138,9 +138,8 @@ module.exports = (env, argv) => {
           Polls: mfeConfigJSON["polls"].url,
           UrlForwarding: mfeConfigJSON["urlForwarding"].url,
           PaymentPlans: mfeConfigJSON["paymentPlans"].url,
-          Blogs: mfeConfigJSON["blogs"].url,
-     BookStore: mfeConfigJSON["bookStore"].url,
-},
+          Blogs: mfeConfigJSON["blogs"].url
+        },
         shared: {
           ...devdeps,
           ...deps,
