3e58a998b541d2ead29f100ec119b24bcbc4accf
Bhorga <harshit.bhorga@waiin.com>
2023-04-04 14:33:11 +0530

    datatable action width

--- raaghu-components/src/rds-comp-data-table/rds-comp-data-table.tsx
+++ raaghu-components/src/rds-comp-data-table/rds-comp-data-table.tsx
@@ -520,7 +520,7 @@ const dragOverItem = useRef<any>(null);
                                      // class="bi bi-three-dots-vertical"
                                     />
                                   </button>
-                                  {array[index] && (<ul className="dropdown-menu" style={{display:'block', maxWidth:'250px'}}>
+                                  {array[index] && (<ul className="dropdown-menu" style={{display:'block', maxWidth:'200px'}}>
                                     {props.actions?.map((action, actionIndex) => (
                                       <li
                                         key={
