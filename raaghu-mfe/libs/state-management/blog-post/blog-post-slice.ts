b15b37ed13717cd0ce31f56601bdcbf59067945a
suraj-khupse <99809670+suraj-khupse@users.noreply.github.com>
2023-04-14 10:26:40 +0530

    blog post page data binding.

--- raaghu-mfe/libs/state-management/blog-post/blog-post-slice.ts
+++ raaghu-mfe/libs/state-management/blog-post/blog-post-slice.ts
@@ -1,80 +1,80 @@
 
 import { createSlice, createAsyncThunk, PayloadAction, } from "@reduxjs/toolkit";
-import { ServiceProxy } from "../../shared/service-proxy";
-import Users from '../../../rds_pages/rds-page-users/src/users/users';
+import { BlogPostAdminService } from "../../proxy/services/BlogPostAdminService";
 
 
-        export interface blogPostInitialState {
-          loading: boolean;
-          blogPosts: [];
-          error: string;
-          allblogpost : [];
-        }; 
+export interface blogPostInitialState {
+  loading: boolean;
+  blogPosts: [];
+  error: string;
+  allblogpost: [];
+};
 
-        export const blogPostState: blogPostInitialState = {
-          loading: false,
-          blogPosts: [],
-          allblogpost : [],
-          error: "",
-        };
+export const blogPostState: blogPostInitialState = {
+  loading: false,
+  blogPosts: [],
+  allblogpost: [],
+  error: "",
+};
 
-        const proxy = new ServiceProxy()
+export const getAllBlogPost = createAsyncThunk('BlogPost/GetAll', () => {
+  return BlogPostAdminService.getBlogsBlogPosts({ filter: undefined, blogId: undefined, authorId: undefined, tagId: undefined, status: undefined, sorting: undefined, skipCount: undefined, maxResultCount: undefined })
+    .then((result: any) => {
+      return result;
+    })
+});
 
-        export const getAllBlogPost = createAsyncThunk('BlogPost/GetAll', () => {
-          return proxy.blogPostsGET(undefined,undefined,undefined,undefined,undefined,undefined,0,1000).then((result: any)=>{
-            return result;
-          })
-        });
+export const addBlogPostData = createAsyncThunk(
+  "BlogPostTypes/addBlogPostData",
+  (BlogPostDto: any) => {
+    return BlogPostAdminService.postBlogsBlogPostsCreateAndPublish(BlogPostDto).then((result: any) => {
+      console.log("Blog post ", result)
+      debugger
+      return result;
+    })
+  }
+);
 
-        export const addBlogPostData = createAsyncThunk(
-          "BlogPostTypes/addBlogPostTypesData",
-          (claimTypeDto: any) => {
-            return proxy.claimTypesPOST(claimTypeDto).then((result:any)=>{
-              return result.items;
-            })
-          }
-        );
+// Add your Api call here
 
-        // Add your Api call here
-
-        const blogPostSlice = createSlice({
-          name: "blogPost",
-          initialState: blogPostState,
-          reducers: {},
-          extraReducers: (builder) => {
-            // Add your extraReducers here 
-           builder.addCase(getAllBlogPost.pending,(state)=>{
-            state.loading = true
-           });
-           builder.addCase(getAllBlogPost.fulfilled,(state, action: PayloadAction<any>)=>{
-            state.loading = false
-            state.allblogpost = action.payload
-            state.error = ''
-           });
-           builder.addCase(getAllBlogPost.rejected, (state, action) => {
-            state.loading = false
-            state.allblogpost = []
-            state.error = action.error.message || 'Something Went Wrong'
-          });
+const blogPostSlice = createSlice({
+  name: "blogPost",
+  initialState: blogPostState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Add your extraReducers here 
+    builder.addCase(getAllBlogPost.pending, (state) => {
+      state.loading = true
+    });
+    builder.addCase(getAllBlogPost.fulfilled, (state, action: PayloadAction<any>) => {
+      state.loading = false
+      state.allblogpost = action.payload
+      state.error = ''
+    });
+    builder.addCase(getAllBlogPost.rejected, (state, action) => {
+      state.loading = false
+      state.allblogpost = []
+      state.error = action.error.message || 'Something Went Wrong'
+    });
 
 
-          builder.addCase(addBlogPostData.pending,(state) =>{
-            state.loading = true;
-          })
+    builder.addCase(addBlogPostData.pending, (state) => {
+      state.loading = true;
+    })
 
-          builder.addCase(addBlogPostData.fulfilled,(state,action: PayloadAction<any>)=>{
-            state.loading = false;
-            state.allblogpost = action.payload;
-            state.error = "";
+    builder.addCase(addBlogPostData.fulfilled, (state, action: PayloadAction<any>) => {
+      state.loading = false;
+      state.allblogpost = action.payload;
+      state.error = "";
 
-          })
+    })
 
-          builder.addCase(addBlogPostData.rejected,(state , action)=>{
-            state.loading = false;
-            state.allblogpost =[];
-            state.error ="";
-          })
-          }})
+    builder.addCase(addBlogPostData.rejected, (state, action) => {
+      state.loading = false;
+      state.allblogpost = [];
+      state.error = "";
+    })
+  }
+})
 
-          export default blogPostSlice.reducer;
-        
+export default blogPostSlice.reducer;
