ad13a91c16eb96b9bec730b8a37379ae96bdebb5
Abhishek Gaurav <gaurav.abhishek99@gmail.com>
2023-04-19 15:04:44 +0530

    Rectified the issue of roles page data table.

--- raaghu-mfe/rds_pages/rds-page-roles/src/Roles/Roles.tsx
+++ raaghu-mfe/rds_pages/rds-page-roles/src/Roles/Roles.tsx
@@ -19,7 +19,7 @@ import {
   useAppSelector,
 } from "../../../../libs/state-management/hooks";
 import {
-  fetchRoles,
+  fetchRolesInRoles,
   addRolesUnit,
   editRoles,
   deleteRoles,
@@ -53,7 +53,7 @@ const Roles = (props: RdsPageRolesProps) => {
 
   
   useEffect(() => {
-    dispatch(fetchRoles() as any);
+    dispatch(fetchRolesInRoles() as any);
     dispatch(fetchAllClaims() as any);
   }, [dispatch]);
   useEffect(() => {
@@ -123,7 +123,7 @@ const Roles = (props: RdsPageRolesProps) => {
           color: "success",
         });
       }
-      dispatch(fetchRoles() as any);
+      dispatch(fetchRolesInRoles() as any);
     });
   };
   useEffect(() => {
@@ -211,7 +211,7 @@ const Roles = (props: RdsPageRolesProps) => {
           color: "success",
         });
       }
-      dispatch(fetchRoles() as any);
+      dispatch(fetchRolesInRoles() as any);
     });
   };
   const handlerEditRole = () => {
@@ -238,7 +238,7 @@ const Roles = (props: RdsPageRolesProps) => {
           color: "success",
         });
       }
-      dispatch(fetchRoles() as any);
+      dispatch(fetchRolesInRoles() as any);
     });
   };
   const handlerNewRole = () => {
@@ -291,7 +291,7 @@ const Roles = (props: RdsPageRolesProps) => {
           onActionSelection={handlerActions}
           recordsPerPageSelectListOption={true}
         ></RdsCompDatatable>
-        <RdsOffcanvas
+        {/* <RdsOffcanvas
           placement="end"
           canvasTitle="New Role"
           
@@ -515,7 +515,7 @@ const Roles = (props: RdsPageRolesProps) => {
               ></RdsButton>
             </div>
           </div>
-        </RdsOffcanvas>
+        </RdsOffcanvas> */}
         <RdsCompAlertPopup
           alertID={`deleteRolesof`}
           onSuccess={handlerDeleteConfirm}
