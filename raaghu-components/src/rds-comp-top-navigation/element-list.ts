0d9f0001c1d0408f4a53f58a729695451a010bb2
Saniya Sonkaria <saniyasonkaria.civ18@itbhu.ac.in>
2023-04-21 17:22:11 +0530

    Implemented Element Page functionality.
    Implemented Element Page functionality.

---
+++ raaghu-components/src/rds-comp-top-navigation/element-list.ts
@@ -0,0 +1,27 @@
+const elementList = [
+    {
+      label: "Accordion",
+      val: "accordion",
+    },
+    {
+      label: "Address Detail",
+      val: "addressDetail",
+    },
+    {
+      label: "Alert",
+      val: "alert",
+    },
+    {
+      label: "App Detail",
+      val: "appDetail",
+    },
+    {
+      label: "Avatar",
+      val: "avatar",
+    },
+    {
+      label: "Badge",
+      val: "badge",
+    },
+  ];
+  export default elementList ;
