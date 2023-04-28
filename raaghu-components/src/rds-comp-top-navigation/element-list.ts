3b949a64e6493388335b7f94aa3887ea1da95701
Saniya Sonkaria <saniyasonkaria.civ18@itbhu.ac.in>
2023-04-28 17:42:58 +0530

    Added Bank-card-detail, Banner, Benefit, Big-number, Breadcrumb ,Button, and  Button-group Elements
    Added Bank-card-detail, Banner, Benefit, Big-number, Breadcrumb ,Button, and  Button-group Elements.

--- raaghu-components/src/rds-comp-top-navigation/element-list.ts
+++ raaghu-components/src/rds-comp-top-navigation/element-list.ts
@@ -1,27 +1,68 @@
 const elementList = [
-    {
-      label: "Accordion",
-      val: "accordion",
-    },
-    {
-      label: "Address Detail",
-      val: "addressDetail",
-    },
-    {
-      label: "Alert",
-      val: "alert",
-    },
-    {
-      label: "App Detail",
-      val: "appDetail",
-    },
-    {
-      label: "Avatar",
-      val: "avatar",
-    },
-    {
-      label: "Badge",
-      val: "badge",
-    },
-  ];
-  export default elementList ;
+  {
+    label: "Accordion",
+    val: "accordion",
+  },
+  {
+    label: "Address Detail",
+    val: "addressDetail",
+  },
+  {
+    label: "Alert",
+    val: "alert",
+  },
+  {
+    label: "App Detail",
+    val: "appDetail",
+  },
+  {
+    label: "Avatar",
+    val: "avatar",
+  },
+  {
+    label: "Badge",
+    val: "badge",
+  },
+  {
+    label: "Bank Card Detail",
+    val: "bankCardDetail",
+  },
+  {
+    label: "Banner",
+    val: "banner",
+  },
+  {
+    label: "Benefit",
+    val: "benefit",
+  },
+  {
+    label: "Big Number",
+    val: "bigNumber",
+  },
+  {
+    label: "Breadcrumb",
+    val: "breadcrumb",
+  },
+  {
+    label: "Button",
+    val: "button",
+  }, {
+    label: "Button Group",
+    val: "buttonGroup",
+  },
+  {
+    label: "Card",
+    val: "card",
+  }
+  ,
+  {
+    label: "carousel",
+    val: "carousel",
+  }
+  ,
+  {
+    label: "Checkbox",
+    val: "checkbox",
+  }
+];
+export default elementList;
