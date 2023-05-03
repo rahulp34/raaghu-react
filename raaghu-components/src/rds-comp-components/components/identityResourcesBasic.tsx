fd06c2decf6785778b8a5c8206443cbb16067d82
Saniya Sonkaria <saniyasonkaria.civ18@itbhu.ac.in>
2023-05-03 18:58:27 +0530

    added more component in component  Page
    added more component in component  Page

---
+++ raaghu-components/src/rds-comp-components/components/identityResourcesBasic.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RdsCompIdentiyResourcseBasic from "../../rds-comp-identiy-resourcse-basic/rds-comp-identiy-resourcse-basic";
+
+export const code_actual = () => {
+  return (
+    <RdsCompIdentiyResourcseBasic/>
+  );
+};
+
+export default code_actual;
