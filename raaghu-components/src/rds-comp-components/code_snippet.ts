cd7d2cbb478e2c0b464a1e62eabf0e67ae1a3482
Saniya Sonkaria <saniyasonkaria.civ18@itbhu.ac.in>
2023-05-04 12:34:40 +0530

    Added code snippet and components till New Role.
    Added code snippet and components till New Role.

--- raaghu-components/src/rds-comp-components/code_snippet.ts
+++ raaghu-components/src/rds-comp-components/code_snippet.ts
@@ -1310,7 +1310,8 @@ BillingAddressDetails={()=>{}}
   />`,
     name: "Information",
   },
-  { integration: `<RdsCompIntegration
+  {
+    integration: `<RdsCompIntegration
   integrationList={[
     {
       title: "Zapier",
@@ -1414,10 +1415,495 @@ BillingAddressDetails={()=>{}}
       iconFill: false,
     },
   ]}
-/>`, name: "Integration" },
+/>`,
+    name: "Integration",
+  },
   {
-    invoice:`<RdsCompInvoice />`,
-    name:"Invoice"
-  }
+    invoice: `<RdsCompInvoice />`,
+    name: "Invoice",
+  },
+  {
+    invoice: `<RdsCompInvoice />`,
+    name: "Invoice",
+  },
+  ,
+  {
+    invoice: `<RdsCompInvoice />`,
+    name: "Invoice",
+  },
+  {
+    invoice: `<RdsCompInvoice />`,
+    name: "Invoice",
+  },
+  {
+    linkedAccount: `<RdsCompLinkedAccount/>`,
+    name: "Linked Account",
+  },
+  {
+    login: `<RdsCompLogin onLogin={() => {}} onForgotPassword={() => {}} />`,
+    name: "Login",
+  },
+  {
+    loginAttempt: `<RdsCompLoginAttempts
+    selectvalue={[
+      { value: "All", displayText: "All" },
+      { value: "Success", displayText: "Success" },
+      {
+        value: "InvalidUserNameOrEmailAddress",
+        displayText: "Invalid Username or email Address",
+      },
+      { value: "InvalidPassword", displayText: "Invalid Password" },
+      { value: "UserIsNotActive", displayText: "User is Not Active" },
+      { value: "InvalidTenancyName", displayText: "Invalid Tenancy name" },
+      { value: "TenantIsNotActive", displayText: "Tenant Is Not Active" },
+      {
+        value: "UserEmailIsNotConfirmed",
+        displayText: "User Email Is Not Confirmed",
+      },
+      {
+        value: "UnknownExternalLogin",
+        displayText: "Unknown External Login",
+      },
+      { value: "LockedOut", displayText: "Locked Out" },
+      {
+        value: "UserPhoneNumberIsNotConfirmed",
+        displayText: "User PhoneNumber IsNot Confirmed",
+      },
+    ]}
+    tableHeaders={[
+      {
+        displayName: "IP Address",
+        key: "ipaddress",
+        datatype: "text",
+        sortable: true,
+      },
+      {
+        displayName: "Clients",
+        key: "icon",
+        datatype: "iconAvatarTitle",
+        //   sortable: true,
+      },
+      {
+        displayName: "Name",
+        key: "name",
+        datatype: "avatarTitleInfo",
+        sortable: true,
+      },
+      {
+        displayName: "Date&Time",
+        key: "time",
+        datatype: "number",
+        sortable: true,
+      },
+      {
+        displayName: "Result",
+        key: "result",
+        datatype: "text",
+        sortable: true,
+      },
+    ]}
+    tableData={[
+      {
+        id: 1,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 60,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/03/2022").toISOString(),
+        result: "InvalidPassword",
+      },
+      {
+        id: 2,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 120,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/04/2022").toISOString(),
+        result: "UserIsNotActive",
+      },
+      {
+        id: 3,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 250,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/05/2022").toISOString(),
+        result: "InvalidPassword",
+      },
+      {
+        id: 4,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 60,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/06/2022").toISOString(),
+        result: "Success",
+      },
+      {
+        id: 5,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/07/2022").toISOString(),
+        result: "UserIsNotActive",
+      },
+      {
+        id: 6,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 60,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/08/2022").toISOString(),
+        result: "Success",
+      },
+      {
+        id: 7,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("11/03/2022").toISOString(),
+        result: "Success",
+      },
+      {
+        id: 8,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("11/04/2022").toISOString(),
+        result: "UserIsNotActive",
+      },
+      {
+        id: 9,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("11/05/2022").toISOString(),
+        result: "Success",
+      },
+      {
+        id: 10,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/02/2022").toISOString(),
+        result: "Success",
+      },
+      {
+        id: 11,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/01/2022").toISOString(),
+        result: "Success",
+      },
+      {
+        id: 12,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("11/09/2022").toISOString(),
+        result: "Success",
+      },
+      {
+        id: 13,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/03/2022").toISOString(),
+        result: "Success",
+      },
+      {
+        id: 14,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/03/2022").toISOString(),
+        result: "Success",
+      },
+      {
+        id: 15,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/03/2022").toISOString(),
+        result: "UserEmailIsNotConfirmed",
+      },
+      {
+        id: 16,
+        name: {
+          avatar:
+            "https://design.firefox.com/product-identity/firefox/firefox/firefox-logo.png",
+          title: "Firefox",
+          info: "Window NT 10",
+        },
+        ipaddress: 100,
+        icon: {
+          iconName: "Computer",
+          iconFill: true,
+          iconStroke: true,
+          iconColor: "dark",
+          iconWidth: "20px",
+          withavatar: false,
+          iconHeight: "20px",
+        },
+        time: new Date("12/03/2022").toISOString(),
+        result: "UserEmailIsNotConfirmed",
+      },
+    ]}
+    onActionSelection={() => {}}
+  />`,
+    name: "Login Attempt",
+  },
+  {
+    editProfile: `<RdsCompProfileEdit />`,
+    name: "Edit Profile",
+  },
+  {
+    mySetting: `<RdsCompMySettings />`,
+    name: "My Setting",
+  },
+  {
+    newClaimType: `<RdsCompNewClaimType onSubmit ={()=>{}}/>`,
+    name: "New Claim Type",
+  },
+  {
+    newLanguage: `<RdsCompNewLanguage
+    onSaveHandler={() => {}}
+    placeholder="Select Country"
+    languageItems={[
+      { option: "Invariant Language ()" },
+      { option: "Afar (aa)" },
+      { option: "Afar (Djibouti) (aa-DJ)" },
+      { option: "Afar (Eritrea) (aa-ER)" },
+      { option: "Afar (Ethiopia) (aa-ET)" },
+      { option: "Afrikaans (af)" },
+      { option: "Afrikaans (Namibia) (af-NA)" },
+      { option: "Afrikaans (South Africa) (af-ZA)" },
+      { option: "Aghem (agq)" },
+      { option: "Aghem (Cameroon) (agq-CM)" },
+      { option: "Akan (ak)" },
+    ]}
+    languageNames={[
+      { option: "Invariant Language ()" },
+      { option: "Afar (aa)" },
+      { option: "Afar (Djibouti) (aa-DJ)" },
+      { option: "Afar (Eritrea) (aa-ER)" },
+      { option: "Afar (Ethiopia) (aa-ET)" },
+      { option: "Afrikaans (af)" },
+      { option: "Afrikaans (Namibia) (af-NA)" },
+      { option: "Afrikaans (South Africa) (af-ZA)" },
+      { option: "Aghem (agq)" },
+      { option: "Aghem (Cameroon) (agq-CM)" },
+      { option: "Akan (ak)" },
+    ]}
+    onClick={undefined}
+    check={false}
+    edit={false}
+  ></RdsCompNewLanguage>`,
+    name: "NewLanguage",
+  },
+  {
+    newProperties: `<RdsCompPropertiesNew />`,
+    name: "New Properties",
+  },
+  {
+    newRole: ` <RdsCompNewRole
+    roleData={{
+      displayName: "Role Name",
+      isDefault: false,
+    }}
+  />`,
+    name: "New Role",
+  },
 ];
 export default code_snippet;
