5cfc9c73512b8b44d1630b880bfdea547443376d
ganesh-mokate-waiin <100756494+ganesh-mokate-waiin@users.noreply.github.com>
2023-05-09 12:06:16 +0530

    Update rds-TextEditor.test.tsx

--- raaghu-elements/__tests__/rds-TextEditor.test.tsx
+++ raaghu-elements/__tests__/rds-TextEditor.test.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-//import { render, screen } from '@testing-library/react';
-//import { RdsTextEditor } from '../src';
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
