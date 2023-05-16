c2f22574953d263583d01e8c8fcc1e634981778e
Abhishek Gaurav <gaurav.abhishek99@gmail.com>
2023-05-16 10:00:57 +0530

    added test file for faq and subscription component.

--- raaghu-components/src/rds-comp-faq/rds-comp-faq.tsx
+++ raaghu-components/src/rds-comp-faq/rds-comp-faq.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RdsLabel } from "raaghu-react-elements";
+import { RdsLabel } from "../rds-elements";
 
 export interface RdsCompFaqProps {
   questionList: any[];
