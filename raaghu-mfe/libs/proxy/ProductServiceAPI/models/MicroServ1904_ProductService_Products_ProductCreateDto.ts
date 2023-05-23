ad1766e31c328b34685ebba0d768cd9efdf0302b
Vikrant singh <Singvikrant21@gmail.com>
2023-05-23 17:28:09 +0530

    Re-commit

--- raaghu-mfe/libs/proxy/ProductServiceAPI/models/MicroServ1904_ProductService_Products_ProductCreateDto.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type MicroServ1904_ProductService_Products_ProductCreateDto = {
-    name: string;
-    price: number;
-};
-
--- raaghu-mfe/libs/proxy/ProductServiceAPI/models/MicroServ1904_ProductService_Products_ProductUpdateDto.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type MicroServ1904_ProductService_Products_ProductUpdateDto = {
-    name: string;
-    price: number;
-};
-
