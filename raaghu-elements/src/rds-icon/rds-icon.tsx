f41528817b0c5f7572ec0b5022de95f5ad739625
Alok Nakhate <alok.nakhate@waiin.com>
2023-05-24 14:12:59 +0530

    Update rds-icon.tsx
    Removed commented code

--- raaghu-elements/src/rds-icon/rds-icon.tsx
+++ raaghu-elements/src/rds-icon/rds-icon.tsx
@@ -24,14 +24,12 @@ const RdsIcon = (props: RdsIconProps) => {
   let icon = Icons.hasOwnProperty(name) ? Icons[name] : Flags[name];
 
   const svgElementFromString = (svgContent: string): SVGElement => {
-    let fillColor = "currentColor";
     const div = document.createElement("DIV");
     div.innerHTML = svgContent;
     const svg = div.querySelector("svg");
     if (!svg) {
       throw Error("<svg> tag not found");
     }
-
     if (props.height) {
       svg.style.height = props.height;
     }
@@ -44,50 +42,6 @@ const RdsIcon = (props: RdsIconProps) => {
     if (props.strokeWidth) {
       svg.style.strokeWidth = props.strokeWidth;
     }
-    // if (props.colorVariant == "primary") {
-    //   // svg.setAttribute('class', 'icon-' + props.colorVariant);
-    //   fillColor = "#7E2EEf";
-    // } else if (props.colorVariant == "secondary") {
-    //   fillColor = "#2B0066";
-    // } else if (props.colorVariant == "success") {
-    //   fillColor = "#2EEF59";
-    // } else if (props.colorVariant == "info") {
-    //   fillColor = "#3ef1e8";
-    // } else if (props.colorVariant == "warning") {
-    //   fillColor = "#E3A300";
-    // } else if (props.colorVariant == "danger") {
-    //   fillColor = "#EF2E2E";
-    // } else if (props.colorVariant == "dark") {
-    //   fillColor = "#363636";
-    // } else if (props.colorVariant == "light") {
-    //   fillColor = "#F8F9FA";
-    // } else if (props.colorVariant == "review") {
-    //   fillColor = "#E3A300";
-    // } else if (props.colorVariant == "basic") {
-    //   fillColor = "#00A443";
-    // } else if (props.colorVariant == "standard") {
-    //   fillColor = "#005FA4";
-    // } else if (props.colorVariant == "premium") {
-    //   fillColor = "#660064";
-    // } else if (props.colorVariant == "professional") {
-    //   fillColor = "#F76161";
-    // }
-
-    // if (props.fill) {
-    //   svg.style.fill = fillColor;
-    // } else {
-    //   svg.style.fill = "none";
-    // }
-
-    // if (props.stroke || props.stroke === undefined) {
-    //   svg.style.stroke = fillColor;
-    // } else {
-    //   svg.style.stroke = "none";
-    // }
-  //   if (props.isAnimate) {
-  //    svg.classList.add("jiggle");
-  //  }
-
     return (
       svg || document.createElementNS("http://www.w3.org/2000/svg", "path")
     );
